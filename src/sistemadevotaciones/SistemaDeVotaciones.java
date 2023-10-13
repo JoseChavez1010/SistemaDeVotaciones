5151306a07b5fa25dd1d9690e89bfa3e696ebb6a
JoseChavez1010 <josechavez2m@gmail.com>
2023-10-12 22:18:30 -0600

    Cambios en los registros de usuarios

--- src/sistemadevotaciones/SistemaDeVotaciones.java
+++ src/sistemadevotaciones/SistemaDeVotaciones.java
@@ -4,7 +4,10 @@ package sistemadevotaciones;
  *
  * @author Joseph
  */
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -19,12 +22,25 @@ public class SistemaDeVotaciones {
 
         System.out.println("Bienvenido al sistema de Votaciones");
         System.out.println("Ingrese una contrasena para el Sistema");
+        
+
+        //declaracion de variables
+        String nombre, apellido, usuario, pass, email, estado, datoModificar, direccion, departamento, municipio;
+        int l;
+        long cui;
 
         File archivo;
+        File datosAdmin;
+        File datosRegVotantes;
+        File datosAuditor;
+        File datosVotantes;
+
         FileWriter escribir;
         PrintWriter linea;
 
         archivo = new File("passSistema.txt");
+        datosAdmin = new File("datosAdmin.txt");
+        datosVotantes = new File("datosVotantes.txt");
 
         if (!archivo.exists()) {
             String passSistem = scan.nextLine();
@@ -33,9 +49,11 @@ public class SistemaDeVotaciones {
                 escribir = new FileWriter(archivo, true);
                 linea = new PrintWriter(escribir);
                 linea.println(passSistem);
+                escribir.close();
+                linea.close();
 
             } catch (IOException ex) {
-            }
+                }
         }
         int opcion1 = 0;
         int i = 1;
@@ -83,10 +101,11 @@ public class SistemaDeVotaciones {
                     } else if (opcR == 6) {
                         i = 6;
                     }
-                    // ciclo para registro de Usuarios;
+                    ;
+
+                    // ciclo para registro de Usuarios;                    
                     int c = 1;
                     while (c < 2) {
-
                         switch (opcR) {
                             case 1:
                                 System.out.println("Sistema de Votaciones");
@@ -97,54 +116,232 @@ public class SistemaDeVotaciones {
                                 System.out.println("4. Regresar");
                                 System.out.println("5. Salir.");
                                 int opcT = scan.nextInt();
+
                                 if (opcT == 4) {
-                                    i = 1;
                                     opcion1 = 2;
+                                    c = 2;
                                 } else if (opcT == 5) {
                                     opcion1 = 3;
+                                    c = 2;
+                                } else {
+
                                 }
-
-                                switch (opcR) {
+                                switch (opcT) {
                                     case 1:
-                                        int d = 0;
-                                        while (d < 2) {
-                                            System.out.println("Ingrese su DPI");
-                                            String dpi = scan.nextLine();
-                                            System.out.println("Ingrese su nombre completo");
-                                            
 
-                                        }// fin while de Registro de Cuenta Administrador
+                                        System.out.println("Ingrese los datos para la Cuenta Administrador");
+                                        scan.nextLine();
+                                        System.out.println("Ingrese Nombres:");
+                                        nombre = scan.nextLine();
+                                        System.out.println("Ingrese Apellidos");
+                                        apellido = scan.nextLine();
+                                        System.out.println("Ingrese No DPI:");
+                                        cui = scan.nextInt();
+                                        System.out.println("Ingrese Su Email:");
+                                        email = scan.nextLine();
+                                        System.out.println("Ingrese Nombre Usuario:");
+                                        usuario = scan.nextLine();
+                                        System.out.println("Ingrese una contraseña:");
+                                        pass = scan.nextLine();
+
+                                        if (!datosAdmin.exists()) {
+                                            try {
+                                                datosAdmin.createNewFile();
+                                                escribir = new FileWriter(datosAdmin, true);
+                                                linea = new PrintWriter(escribir);
+                                                linea.print(cui + "|");
+                                                linea.print(nombre + "|");
+                                                linea.print(apellido + "|");
+                                                linea.print(email + "|");
+                                                linea.print(usuario + "|");
+                                                linea.print(pass + "|");
+                                                linea.print("habilitado");
+                                                linea.print("\n");
+                                                escribir.close();
+                                                linea.close();
+                                            } catch (IOException ex) {
+                                                }
+                                        } else {
+                                            try {
+                                                escribir = new FileWriter(datosAdmin, true);
+                                                linea = new PrintWriter(escribir);
+                                                linea.print(cui + "|");
+                                                linea.print(nombre + "|");
+                                                linea.print(apellido + "|");
+                                                linea.print(email + "|");
+                                                linea.print(usuario + "|");
+                                                linea.print(pass + "|");
+                                                linea.print("habilitado");
+                                                linea.print("\n");
+                                                escribir.close();
+                                                linea.close();
+                                            } catch (IOException ex) {
+                                            }
+                                        }
+                                        ;
+                                        System.out.println("Usuario Creado Exitosamente");
+                                        break;
+                                    case 2:
+                                        System.out.println("Ingrese los datos para la cuenta de Registrador de Votantes");
+                                        System.out.println("Ingrese su nombre Completo");
+                                        scan.nextLine();//limpiar buffer
+                                        System.out.println("Ingrese Nombres:");
+                                        nombre = scan.nextLine();
+                                        System.out.println("Ingrese Apellidos");
+                                        apellido = scan.nextLine();
+                                        System.out.println("Ingrese No DPI:");
+                                        cui = scan.nextLong();
+                                        System.out.println("Ingrese Su Email:");
+                                        email = scan.nextLine();
+                                        System.out.println("Ingrese Nombre Usuario:");
+                                        usuario = scan.nextLine();
+                                        System.out.println("Ingrese una contraseña:");
+                                        pass = scan.nextLine();
+
+                                        if (!datosAdmin.exists()) {
+                                            try {
+                                                datosAdmin.createNewFile();
+                                                escribir = new FileWriter(datosAdmin, true);
+                                                linea = new PrintWriter(escribir);
+                                                linea.print(cui + "|");
+                                                linea.print(nombre + "|");
+                                                linea.print(apellido + "|");
+                                                linea.print(email + "|");
+                                                linea.print(usuario + "|");
+                                                linea.print(pass + "|");
+                                                linea.print("habilitado");
+                                                linea.print("\n");
+                                                escribir.close();
+                                                linea.close();
+                                            } catch (IOException ex) {
+                                              }
+                                        } else {
+                                            try {
+                                                escribir = new FileWriter(datosAdmin, true);
+                                                linea = new PrintWriter(escribir);
+                                                linea.print(cui + "|");
+                                                linea.print(nombre + "|");
+                                                linea.print(apellido + "|");
+                                                linea.print(email + "|");
+                                                linea.print(usuario + "|");
+                                                linea.print(pass + "|");
+                                                linea.print("habilitado");
+                                                linea.print("\n");
+                                                escribir.close();
+                                                linea.close();
+                                            } catch (IOException ex) {
+                                            }
+                                        }
+                                        ;
+                                        System.out.println("Usuario Creado Exitosamente");
+                                        break;
+                                    case 3:
+                                        System.out.println("Ingrese los datos para la Cuenta Auditor");
+                                        System.out.println("Ingrese Nombre Completo:");
+                                        scan.nextLine();
+                                        System.out.println("Ingrese Nombres:");
+                                        nombre = scan.nextLine();
+                                        System.out.println("Ingrese Apellidos");
+                                        apellido = scan.nextLine();
+                                        System.out.println("Ingrese No DPI:");
+                                        cui = scan.nextLong();
+                                        System.out.println("Ingrese Su Email:");
+                                        email = scan.nextLine();
+                                        System.out.println("Ingrese Nombre Usuario:");
+                                        usuario = scan.nextLine();
+                                        System.out.println("Ingrese una contraseña:");
+                                        pass = scan.nextLine();
+
+                                        if (!datosAdmin.exists()) {
+                                            try {
+                                                datosAdmin.createNewFile();
+                                                escribir = new FileWriter(datosAdmin, true);
+                                                linea = new PrintWriter(escribir);
+                                                linea.print(cui + "|");
+                                                linea.print(nombre + "|");
+                                                linea.print(apellido + "|");
+                                                linea.print(email + "|");
+                                                linea.print(usuario + "|");
+                                                linea.print(pass + "|");
+                                                linea.print("habilitado");
+                                                linea.print("\n");
+                                                escribir.close();
+                                                linea.close();
+                                            } catch (IOException ex) {
+                                                 }
+                                        } else {
+                                            try {
+                                                escribir = new FileWriter(datosAdmin, true);
+                                                linea = new PrintWriter(escribir);
+                                                linea.print(cui + "|");
+                                                linea.print(nombre + "|");
+                                                linea.print(apellido + "|");
+                                                linea.print(email + "|");
+                                                linea.print(usuario + "|");
+                                                linea.print(pass + "|");
+                                                linea.print("habilitado");
+                                                linea.print("\n");
+                                                escribir.close();
+                                                linea.close();
+                                            } catch (IOException ex) {
+                                            }
+                                        }
+                                        ;
+                                        System.out.println("Usuario Creado Exitosamente");
+                                        break;
+                                    case 4:
+                                        c = 2;
+                                        opcion1 = 2;
+                                        break;
+                                    case 5:
+                                        i = 2;
+                                        break;
+                                    default:
                                         break;
                                 }
-                                c = 6;
-                                break;
+                                ;
+                                break;// fin primer caso                            
                             case 2:
                                 System.out.println("Sistema de Votaciones");
                                 System.out.println("Restablecer Contraseña");
                                 System.out.println("Ingrese su Email");
-                                String email = scan.nextLine();
+                                email = scan.nextLine();
+
+                                c = 2;
                                 break;
+                            //fin segundo caso
                             case 3:
                                 System.out.println("Sistema de Votaciones");
                                 System.out.println("Modificar Usuario");
                                 System.out.println("Ingrese su Email");
+                                scan.nextLine();
                                 email = scan.nextLine();
                                 System.out.println("Ingrese Contraseña");
-                                String pass = scan.nextLine();
+                                pass = scan.nextLine();
+                                try {
+                                    FileReader leer = new FileReader(datosVotantes);
+                                    BufferedReader br = new BufferedReader(leer);
+                                    String d = "";
+                                    while ((d = br.readLine()) != null) {
+                                        System.out.println(d);
+                                    }
+                                    br.close();
+                                } catch (FileNotFoundException ex) {
+                                    } catch (IOException ex) {
+                                    }
+                                c = 2;
                                 break;
+                            //fin tercer caso
                             case 4:
                                 System.out.println("Sistema de Votaciones");
                                 System.out.println("Desabilitar Usuario");
                                 System.out.println("Ingrese su DPI");
                                 System.out.println("Ingrese pass");
-
-                        };
-                        //finalizacion de switch Tipo de Usuario
-                    }
-                    ;
-                    // finalizacion de while Tipo de Usuario;
-
+                                break;
+                        }
+                    } // cierre de switch y while
                     break;
+                //fin segundo caso
                 case 3:
                     i = 5;
                     break;
